8486647d550e923830156b22d15ca7c4aa3dab68
mauro <maurop123@gmail.com>
2018-04-28 03:36:34 -0400

    remove footer

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,6 @@
     <v-content>
       <router-view/>
     </v-content>
-    <v-footer class="secondary" app>
-      <v-layout row wrap align-center>
-        <v-flex xs12>
-          <div class="white--text ml-3">
-            Theme made with
-            <v-icon class="red--text">favorite</v-icon>
-            by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>,
-            <a class="white--text" href="https://github.com/vwxyzjn" target="_blank">Costa Huang</a>,
-            and Mauro too ;-)
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-footer>
   </v-app>
 </template>
 
